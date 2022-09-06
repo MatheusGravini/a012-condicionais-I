61bc489ef13eda89729cbaaf7a4315010a0be96b
MatheusGravini <gravini.mello@gmail.com>
2022-09-06 17:32:27 -0300

    Exercício condicionais-I

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,8 +2,24 @@ let idade;
 let isMaiorIdade;
 let isCursandoOutraFaculdade;
 
-if(){
+let idade1 = +prompt("Insira aqui sua idade!")
+let terminouEnsinoM1 = prompt("Você terminou o ensino médio?")
+let cursandoFaculdade = prompt("Você está cursando a faculdade?")
 
-}else{
-	
+if(idade1 >= 18){
+ console.log("Maior de idade")
+} else { 
+ console.log("Menor de idade")
+}
+
+if(terminouEnsinoM1 === "sim" && idade1 >= 17){
+    console.log("Concluiu o Ensino Médio!")
+} else {
+    console.log("Não concluiu o Ensino Médio!")
+}
+
+if(cursandoFaculdade === "sim" && terminouEnsinoM1 === "sim"){
+    console.log("Está cursando alguma faculdade")
+} else {
+    console.log("Não está cursando nenhuma faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,66 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLocaleLowerCase()
+
+if(nacionalidade === "brasileira"){
+         console.log("brasileira")
+     } else if (nacionalidade === "uruguaia") {
+         console.log("uruguaia")
+     } else if (nacionalidade === "argentina") {
+        console.log("argentina")
+     } else if (nacionalidade === "chilena"){
+            console.log("chilena")
+     } else if (nacionalidade === "colombiana"){
+                console.log("colombiana")
+     } else {
+        console.log("Nacionalidade não encontrada")
+     }
+
+
+
+
+// if(nacionalidade === "Brasileira"){
+//     console.log("Brasileira")
+// } else {
+//     console.log("Nacionalidade não encontrada")
+// }
+
+// if(nacionalidade === "Argentina"){
+//     console.log("Argentina")
+// } else {
+//     console.log("Nacionalidade não encontrada")
+// }
+
+// if(nacionalidade === "Uruguaia"){
+//     console.log("Uruguaia")
+// } else {
+//     console.log("Nacionalidade não encontrada")
+// }
+
+// if(nacionalidade === "Chilena"){
+//     console.log("Chilena")
+// } else {
+//     console.log("Nacionalidade não encontrada")
+// }
+
+// if(nacionalidade === "Colombiana"){
+//     console.log("Colombiana")
+// } else {
+//     console.log("Nacionalidade não encontrada")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
